1c5b2adde805d5d7a7814b913e3ca395bee54575
Andreas Lindahl Flåten <andreas.flaten@itk.ntnu.no>
2017-02-15 16:58:56 +0100

    Cosmetic changes in plot_example and moved print_simulink

--- code/plot_example.m
+++ code/plot_example.m
@@ -1,19 +1,19 @@
-%% Dummy signals 
+%% Example data
 t = linspace(0,5,200);
 
 y1 = 2*sin(3*t + 2);
-y2 = y1 + randn(size(t));
+y2 = y1 + 0.3*randn(size(t));
 
-%%
+%% Define figure size
 width = 10; % cm
 height = 10; % cm
-fontsize = 10; %points
+fontsize = 10; % points
+x = 20; y = 20; % Where on the screen the plot will appear, not important.
 
-set(0,'DefaultTextInterpreter', 'latex') %Interpret (most) text as latex. Since we use set(0,... this is a global setting
+set(0,'DefaultTextInterpreter', 'latex') % Interpret (most) text as LaTeX.
 
 %% Set up the figure
-fig1 = figure(1); %get the figure handle for this specific figure (figure 1), to set figure-specific properties.
-x = 20; y= 20; %Where on the screen the plot will appear, not important
+fig1 = figure(1);
 fig1.Units = 'centimeters';
 fig1.Position = [x y width height];
 
@@ -33,16 +33,16 @@ ax.TickLabelInterpreter = 'latex'; %Interpret Tick labels as latex
 % Label the axes, set up legend and title
 xlabel('Time [s]')
 ylabel('$\psi$ [rad]')
-legend('Filtered \psi', 'Measured \psi', 'Location','SouthEast')
+legend('Filtered \psi', 'Measured \psi', 'Location','NorthEast')
 title('Heading')
 ax.TitleFontSizeMultiplier = 1.1; %Set title to be 1.1 times larger than other fonts
 
+%% Export the figure
 hgexport(fig1,'heading_plot.eps')
-%optional to save the fig, for later customization: 
+% optional: save the fig, for later customization: 
 % saveas(fig1,'heading_plot.fig')
-%optional to save the data, for later plot reproduction:
+% optional: save the data, for later plot reproduction:
 % save('myAwesomeData.mat','t','y1','y2');
-
 
 %% (not important) Uncomment these lines if you want to set up different settings for the exported figure than for the figure plotted in matlab
 % plotStyle = hgexport('factorystyle');
